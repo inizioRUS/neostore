c05f635524af37076631097a1534ba15fbba9637
o1egs <tankistonline21@gmail.com>
2023-06-08 16:07:13 +0400

    back

---
+++ src/main/java/ru/bebriki/bebriki/BebrikiApplication.java
@@ -0,0 +1,12 @@
+package ru.bebriki.bebriki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebrikiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BebrikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bebriki/bebriki/BebrikiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bebriki.bebriki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebrikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
