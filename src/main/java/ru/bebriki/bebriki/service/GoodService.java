1e2fd33bf534ef4acf2a0b5ef493783ab8e8ed42
student64 <bloodstar215@gmail.com>
2023-06-08 17:25:14 +0400

    Базовая Repo2

---
+++ src/main/java/ru/bebriki/bebriki/service/GoodService.java
@@ -0,0 +1,43 @@
+package ru.bebriki.bebriki.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.bebriki.bebriki.models.Good;
+
+import java.util.List;
+
+@Service
+public class GoodService {
+    @Autowired
+    private GoodRepository repository;
+
+    public Good saveGood(Good good){
+        return repository.save(good);
+    }
+
+    public List<Good> saveGoods(List<Good> goods){
+        return repository.saveAll(goods);
+    }
+    public List<Good> getGoods(){
+        return repository.findAll();
+    }
+    public Good getGoodById(int id){
+        return repository.findById(id).orElse(null);
+    }
+    public List<Good> getGoodsByName(String name){
+        return repository.findByName(name).orElse(null);
+    }
+    public String deleteGood(int id){
+        repository.deleteById(id);
+        return "Good has been deleted "+ id;
+    }
+    public Good updateGood(Good good){
+        Good existingGood = repository.findById(good.getId()).orElse(null);
+        existingGood.setTitle(good.getTitle());
+        existingGood.setAmount(good.getAmount());
+        existingGood.setPrice(good.getPrice());
+        return repository.save(existingGood);
+
+    }
+
+}
