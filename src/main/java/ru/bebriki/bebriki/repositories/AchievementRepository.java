48f3df2055c5cb7381f589e9145e901981b27f89
SamiyKlassniy <kishko.2003@list.ru>
2023-06-08 17:09:42 +0400

    adding some repos with realisation

--- src/main/java/ru/bebriki/bebriki/repositories/AchievementRepository.java
+++ src/main/java/ru/bebriki/bebriki/repositories/AchievementRepository.java
@@ -1,6 +1,7 @@
 package ru.bebriki.bebriki.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.bebriki.bebriki.models.Achievement;
 
 public interface AchievementRepository extends JpaRepository<Achievement, Integer> {
 }
--- src/main/java/ru/bebriki/bebriki/repositories/OrderItemRepository.java
+++ src/main/java/ru/bebriki/bebriki/repositories/OrderItemRepository.java
@@ -1,6 +1,7 @@
 package ru.bebriki.bebriki.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.bebriki.bebriki.models.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
 }
