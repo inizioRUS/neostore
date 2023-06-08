5632d1be7033e7bb93404ad23643a2b9af672d3c
SamiyKlassniy <kishko.2003@list.ru>
2023-06-08 17:26:15 +0400

    adding some repos with realisation v2

--- src/main/java/ru/bebriki/bebriki/controller/GoodController.java
+++ src/main/java/ru/bebriki/bebriki/controller/GoodController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.bebriki.bebriki.models.Good;
+
+import java.util.List;
 
 @RestController
 public class GoodController {
@@ -14,14 +17,17 @@ public class GoodController {
     public Good addGood(@RequestBody Good good){
         return service.saveGood(good);
     }
+
     @PostMapping("/addGoods")
     public List<Good> addGoods(@RequestBody List<Good> goods){
         return service.saveGoods(goods);
     }
+
     @GetMapping("/goods")
     public List<Good> findAllGoods(){
         return service.getGoods();
     }
+
     @GetMapping("/goodById/{id}")
     public Good findGoodById(@PathVariable int id){
         return service.getGoodById(id);
@@ -31,10 +37,12 @@ public class GoodController {
     public List<Good> findGoodsByName(@PathVariable String name){
         return service.getGoodsByName(name);
     }
+
     @PutMapping("/update")
     public Good updateGood(@RequestBody Good good){
         return service.updateGood(good);
     }
+
     @DeleteMapping("/delete/{id}")
     public String deleteGood(@PathVariable int id){
         return service.deleteGood(id);
