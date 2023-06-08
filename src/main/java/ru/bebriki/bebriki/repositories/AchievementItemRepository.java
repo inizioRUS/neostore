70149a964454b99ab112a81439d8bb9590a07546
SamiyKlassniy <kishko.2003@list.ru>
2023-06-08 17:04:55 +0400

    adding some repos

--- src/main/java/ru/bebriki/bebriki/repositories/AchievementItemRepository.java
+++ src/main/java/ru/bebriki/bebriki/repositories/AchievementItemRepository.java
@@ -2,5 +2,5 @@ package ru.bebriki.bebriki.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AchievementListRepository extends JpaRepository<AchievementList, Integer> {
+public interface AchievementItemRepository extends JpaRepository<AchievementItem, Integer> {
 }
--- src/main/java/ru/bebriki/bebriki/repositories/GoodRepository.java
+++ src/main/java/ru/bebriki/bebriki/repositories/GoodRepository.java
@@ -1,6 +1,10 @@
 package ru.bebriki.bebriki.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.bebriki.bebriki.models.Good;
 
 public interface GoodRepository extends JpaRepository<Good, Integer> {
+
+    Good findByName(String name);
+
 }
