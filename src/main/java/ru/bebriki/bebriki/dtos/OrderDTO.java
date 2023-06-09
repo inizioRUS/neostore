d0d94237131c24eaeefcfaa637973ad53aed796b
student64 <bloodstar215@gmail.com>
2023-06-09 14:53:02 +0400

    OrderDTO

---
+++ src/main/java/ru/bebriki/bebriki/dtos/OrderDTO.java
@@ -0,0 +1,26 @@
+package ru.bebriki.bebriki.dtos;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.bebriki.bebriki.models.OrderItem;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+    private Integer id;
+    private Integer workerId;
+    private LocalDateTime date;
+    private List<OrderItem> items;
+
+}
