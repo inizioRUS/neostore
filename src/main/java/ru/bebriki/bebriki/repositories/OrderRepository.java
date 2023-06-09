312f1529f1f045d73fd6d2133bccbf5bde790802
student64 <bloodstar215@gmail.com>
2023-06-09 14:54:42 +0400

    OrderRepository

--- src/main/java/ru/bebriki/bebriki/repositories/OrderRepository.java
+++ src/main/java/ru/bebriki/bebriki/repositories/OrderRepository.java
@@ -2,6 +2,11 @@ package ru.bebriki.bebriki.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.bebriki.bebriki.models.Order;
+import ru.bebriki.bebriki.models.Task;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Optional<Order> findByData(LocalDateTime date);
 }
