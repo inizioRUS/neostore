f46532a7b6b493f372e012e93456cb9e79693ac8
SamiyKlassniy <kishko.2003@list.ru>
2023-06-10 01:04:51 +0400

    pohui proebali v2

--- src/main/java/ru/bebriki/bebriki/controller/TaskController.java
+++ src/main/java/ru/bebriki/bebriki/controller/TaskController.java
@@ -7,12 +7,6 @@ import ru.bebriki.bebriki.dtos.TaskDTO;
 import ru.bebriki.bebriki.models.Task;
 import ru.bebriki.bebriki.models.Worker;
 import ru.bebriki.bebriki.service.TaskService;
-
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -49,38 +43,8 @@ public class TaskController {
     }
 
     @GetMapping("/taskByDifficulty")
-    public List<Task> getTaskByDifficulty(@RequestParam("difficulty") int difficulty) throws TaskNotFoundException{
+    public List<Task> getTaskByDifficulty(@RequestParam("difficulty") int difficulty) {
         return taskService.getTaskByDifficulty(difficulty);
     }
 
-    public int addBalance(Task task){
-        if(task.getDifficulty()==1) return 5;
-        if(task.getDifficulty()==2) return 15;
-        return 25;
-    }
-    public int decreaseBalance(Task task){
-        if(task.getDifficulty()==1) return 12;
-        if(task.getDifficulty()==2) return 7;
-        return 3;
-    }
-    public boolean isDone(){
-        if(this.isDone()) return true;
-        return false;
-    }
-//    public void changeBalance(Worker worker){
-//        Task task = worker;
-//        if(task.getIsDone()){
-//            worker.setBalance(worker.getBalance()+addBalance(task));
-//        }
-//        else worker.setBalance(worker.getBalance()-decreaseBalance(task));
-//    }
-//    public void getDayNumberOld(List<Worker> workers) {
-//        for(Worker w:workers){
-//            LocalDate date = w.getTask().getDate();
-//            if(date!=LocalDate.now()){
-//                DayOfWeek day = date.getDayOfWeek();
-//                if(day==DayOfWeek.MONDAY) changeBalance(w);
-//            }
-//        }
-//    }
 }
